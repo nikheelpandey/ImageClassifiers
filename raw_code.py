20168ef8ad6604ab2088874d805f29f48193ef3e
Nikhil Pandey <nikhilpandey361@gmail.com>
2017-02-27 23:00:14 +0530

    Create raw_code.py

---
+++ raw_code.py
@@ -0,0 +1,155 @@
+
+# coding: utf-8
+
+# In[55]:
+
+import cv2
+import tensorflow
+import tflearn
+import numpy as np 
+import os
+
+from tqdm import tqdm
+from random import shuffle
+
+TRAIN_DIR = '/home/blackperl/Downloads/train'
+TEST_DIR = '/home/blackperl/Downloads/test'
+IMG_SIZE= 50
+LR = 1e-3
+MODEL_NAME = 'dogvscats-{}-{}.model'.format (LR, '2conv-basic-video')
+
+
+# In[56]:
+
+def label_img(img):
+	word_label = img.split('.')[-3]
+	if word_label == 'cat' : return [1,0]
+	elif word_label == 'dog': return [0,1]
+
+
+# In[57]:
+
+def create_train_data():
+	training_data = []
+	for img in tqdm(os.listdir(TRAIN_DIR)):
+		label = label_img(img)
+		path = os.path.join(TRAIN_DIR, img)
+		img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
+		training_data.append([np.array(img), np.array(label)])
+        shuffle(training_data)
+	np.save('train_data.npy', training_data)
+	return training_data
+
+
+# In[58]:
+
+def process_test_data():
+	testing_data = []
+	for img in tqdm(os.listdir(TEST_DIR)):
+		path = os.path.join(TEST_DIR, img)
+		img_num = img.split('.')[0]
+		img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
+		training_data.append([np.array(img), img_num])
+
+	np.save('test_data.npy', testing_data)
+	return testing_data
+
+
+# In[59]:
+
+train_data = create_train_data()
+
+
+# In[60]:
+
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
+convnet = input_data(shape=[None, 50, 50, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+
+# In[61]:
+
+if os.path.exists('{}.meta'.format(MODEL_NAME)):
+	model.load(MODEL_NAME)
+	print('modle loaded!')
+train = train_data[:-500]
+test = train_data[-500:]
+X= np.array([i[0] for i in train]).reshape(-1, 50, 50, 1)
+Y= [i[1] for i in train]
+
+test_x= np.array([i[0] for i in test]).reshape(-1, 50, 50, 1)
+test_y= [i[1] for i in test]
+
+
+
+# In[68]:
+
+model.fit({'input': X}, {'targets': Y}, n_epoch=5, validation_set=({'input': test_x}, {'targets': test_y}), snapshot_step=50, 
+          show_metric=True, run_id=MODEL_NAME)
+    
+
+
+import matplotlib.pyplot as plt
+
+test_data = process_test_data()
+test_data = np.load("test_data.npy")
+fig= plt.figure()
+
+
+for num, data in enumerate(test_data[:12]):
+	img_num = data[1]
+	img_data= data[0]
+	
+	
+	y = fig.add_subplot(3,4,num+1)
+	orig = img_data
+	data= img_data.reshape(50,50,1)
+	model_out = model.predict([data])[0]
+	
+	if np.argmax(model_out) == 1: str_label ='Dog'
+	else: str_label = 'Cat'	
+
+	y.imshow(orig, cmap='gray')
+	plt.title(str_label)
+	y.axes.get_xaxis().set_visible(False)
+	y.axes.get_yaxis().set_visible(False)
+plt.show()
+
+with open('submission-file.csv', 'w') as p:
+	p.write('id,label\n')
+#the following segment of code is basically the saving of result in a csv and it is throwing so mainy disgusting errors!
+with open('submission-file.csv','a') as p:
+	for data in tqdm(test_data):
+		img_num = data[1]
+		img_data= data[0]
+		orig = img_data
+		data= img_data.reshape(50,50,1)
+		model_out = model.predict([data])[0]
+		f.write('{},{}\n'.format(img_num, model_out[1]))
+	
+		
+
+
+
+
+
+
+
+
